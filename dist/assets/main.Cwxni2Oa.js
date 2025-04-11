181a8a2e2a6d8771e242a133c107e5150c3f9c0d
senjucao <331360294@qq.com>
2025-04-11 13:08:35 +0800

    git

--- dist/assets/main.Cwxni2Oa.js
+++
@@ -1,1 +0,0 @@
-import"./main-lIldaq5A.js";import"./index-hifc6bGH.js";
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       filename: 'service-worker.ts',
     }),
   ],
-  base: '/webhid/',
+  base: './',
   worker: {
     format: "es"
   },
