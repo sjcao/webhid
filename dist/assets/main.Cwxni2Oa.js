e746bd15f02bc99badf78598217a755f68964ea2
senjucao <331360294@qq.com>
2025-04-11 13:00:04 +0800

    fix

---
+++ dist/assets/main.Cwxni2Oa.js
@@ -0,0 +1,1 @@
+import"./main-lIldaq5A.js";import"./index-hifc6bGH.js";
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       filename: 'service-worker.ts',
     }),
   ],
-  base: './',
+  base: '/webhid/',
   worker: {
     format: "es"
   },
